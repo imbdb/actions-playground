8f91efcefc45ba4912fd911a1834e8882d3a0daf
Bharat D Bhadresha <bharatdb.me@gmail.com>
2022-12-06 22:26:49 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+declare -A associativeArray=(["master"]="prod" ["integration"]="int" ["staging"]="staging")
+branch=`git branch --show`
+if [[ -v "associativeArray[$branch]" ]] ; then
+  env="${associativeArray[$branch]}"
+else
+  env="FALSE"
+fi
+
+declare -A branchkeymap=(["master"]="PLASMO_PROD_KEYS" ["integration"]="PLASMO_INT_KEYS" ["staging"]="PLASMO_STAGING_KEYS")
+branch=`git branch --show`
+if [[ -v "branchkeymap[$branch]" ]] ; then
+  secretkey="${branchkeymap[$branch]}"
+else
+  secretkey="FALSE"
+fi
+
+echo "ENV=$env,SECRETKEY=$secretkey"
